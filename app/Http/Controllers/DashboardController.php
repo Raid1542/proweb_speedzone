6a028370b495e20f853f780447671ad479ed621f
Frimaliaa02 <lyaalii51@gmail.com>
2025-04-19 10:37:59 +0700

    Menambahkan link tailwind pada salah satu view

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,7 +6,14 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    function dashboard ($nama, $warna, $harga, $stok){
-        return view('dashboard', ['nama' => $nama, 'warna' => $warna, 'harga' => $harga, 'stok' => $stok]);
+    public function index()
+    {
+        // Data dummy yang diubah jadi warna dan stok
+        $userCount = 125;
+        $warna = "Merah, Biru, Hitam";
+        $stok = 58;
+    
+        return view('dashboard', compact('userCount', 'warna', 'stok'));
     }
+    
 }
